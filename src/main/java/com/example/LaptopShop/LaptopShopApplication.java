54a8a9396e7f318c3591cd428aad2ddec2f707d0
ManhHung03 <tranmanhhung9294@gmail.com>
2025-04-11 08:07:35 +0700

    update

--- src/main/java/com/example/LaptopShop/LaptopShopApplication.java
+++ src/main/java/com/example/LaptopShop/LaptopShopApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LaptopShopApplication {
-	// test
+
 	public static void main(String[] args) {
 		SpringApplication.run(LaptopShopApplication.class, args);
 	}
